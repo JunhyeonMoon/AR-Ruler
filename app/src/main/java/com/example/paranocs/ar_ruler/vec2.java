eb5e86b7b811488267adc3cf9eac08f047cba99a
Junhyeon Moon <paranocs@gmail.com>
2018-11-28 18:28:24 +0900

    initial commit

---
+++ app/src/main/java/com/example/paranocs/ar_ruler/vec2.java
@@ -0,0 +1,11 @@
+package com.example.paranocs.ar_ruler;
+
+public class vec2 {
+    public float x;
+    public float y;
+
+    vec2(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+}
---
+++ app/src/main/jni/opencv_java3.c
@@ -0,0 +1,12 @@
+#include <jni.h>
+
+JNIEXPORT void JNICALL
+(JNIEnv *env, jobject instance,
+                                                                  jlong matAddrInput,
+                                                                  jlong matAddrResult, jint a,
+                                                                  jint b, jint c, jfloat x1,
+                                                                  jfloat y1, jfloat x2, jfloat y2) {
+
+    // TODO
+
+}
